fc04a0842f960e3555380c8124376066b1d54b28
Adam Dunn <asdunn18@gmail.com>
2023-01-09 18:53:21 -0500

    old but new

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
-
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
+close.addEventListener('click', () => container.classList.remove('show-nav'))
 
 
 
