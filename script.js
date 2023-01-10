b0eba0aa7da32dbeb5000f5d3caf691c65dc9216
Adam Dunn <asdunn18@gmail.com>
2023-01-10 08:55:00 -0500

    Update Formatting

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
-
-open.addEventListener('click', () => container.classList.add('show-nav'))
-
-close.addEventListener('click', () => container.classList.remove('show-nav'))
-
-
-
-
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 2000,
 	speedAsDuration: true
